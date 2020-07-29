8e5704041af57691629e26369304125fe711b092
Matthew Mattox <mmattox@support.tools>
2020-07-28 23:27:36 -0500

    Fixing build script

--- server/build.sh
+++ server/build.sh
@@ -15,13 +15,13 @@ echo "Starting server build process..."
 cd ./server
 echo "Starting docker build..."
 docker build -t "$DOCKER_REGISTRY_PATH_AGENT":"$BRANCH"."$BUILD_NUMBER" .
-if [ $? -eq 0 ];
+if [ ! $? -eq 0 ];
 then
   echo "Docker build failed"
   exit 2
 fi
 docker push "$DOCKER_REGISTRY_PATH_AGENT":"$BRANCH"."$BUILD_NUMBER"
-if [ $? -eq 0 ];
+if [ ! $? -eq 0 ];
 then
   echo "Docker push failed"
   exit 2
