f687cdbc8b3d77bbb24fa3fd0cc52a6d71c64618
Matthew Mattox <mmattox@support.tools>
2020-07-28 23:19:41 -0500

    Removing DOCKER_REGISTRY_PATH

--- build/main.sh
+++ build/main.sh
@@ -7,9 +7,8 @@ echo "BRANCH: $BRANCH"
 echo "KUBE_CONTEXT: $KUBE_CONTEXT"
 echo "DB_SCALE: $DB_SCALE"
 echo "PROJECT_ID: $PROJECT_ID"
-echo "DOCKER_REGISTRY_PATH: $DOCKER_REGISTRY_PATH"
 
-if [[ -z $GIT_COMMIT ]] || [[ -z $GIT_USERNAME ]] || [[ -z $BRANCH ]] || [[ -z $KUBE_CONTEXT ]] || [[ -z $DB_SCALE ]] || [[ -z $PROJECT_ID ]] || [[ -z $DOCKER_REGISTRY_PATH ]]
+if [[ -z $GIT_COMMIT ]] || [[ -z $GIT_USERNAME ]] || [[ -z $BRANCH ]] || [[ -z $KUBE_CONTEXT ]] || [[ -z $DB_SCALE ]] || [[ -z $PROJECT_ID ]]
 then
   echo "Missing environment variables"
   exit 1
